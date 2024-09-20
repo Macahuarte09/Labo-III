78544176fe5e6c434cd99265df7eefd3998ad701
MacaHuarte09 <macaahuarte09@gmail.com>
2024-09-20 10:09:31 -0300

    actualizacion dto

--- src/main/java/ar/edu/utn/frbb/tup/controller/dto/CuentaDto.java
+++ src/main/java/ar/edu/utn/frbb/tup/controller/dto/CuentaDto.java
@@ -1,78 +1,33 @@
 package ar.edu.utn.frbb.tup.controller.dto;
-
-import java.time.LocalDateTime;
-
+import java.util.Random;
 public class CuentaDto {
-    private long numeroCuenta;
-    private LocalDateTime fechaCreacion;
-    private double balance;
+
     private String tipoCuenta;
     private String moneda;
     private long dniTitular;
-    private String nombreTitular;
-    private String apellidoTitular;
+    private double balance;
 
-    public long getNumeroCuenta() {
-        return numeroCuenta;
-    }
+    public long getDniTitular() {return dniTitular;}
 
-    public void setNumeroCuenta(long numeroCuenta) {
-        this.numeroCuenta = numeroCuenta;
-    }
-
-    public LocalDateTime getFechaCreacion() {
-        return fechaCreacion;
-    }
-
-    public void setFechaCreacion(LocalDateTime fechaCreacion) {
-        this.fechaCreacion = fechaCreacion;
-    }
-
-    public double getBalance() {
-        return balance;
-    }
-
-    public void setBalance(double balance) {
-        this.balance = balance;
+    public String getMoneda() {
+        return moneda;
     }
 
     public String getTipoCuenta() {
         return tipoCuenta;
     }
 
-    public void setTipoCuenta(String tipoCuenta) {
-        this.tipoCuenta = tipoCuenta;
-    }
+    public double getBalance() {return balance;}
 
-    public String getMoneda() {
-        return moneda;
-    }
+    public void setDniTitular(long dniTitular) {this.dniTitular = dniTitular;}
 
     public void setMoneda(String moneda) {
         this.moneda = moneda;
     }
 
-    public long getDniTitular() {
-        return dniTitular;
+    public void setTipoCuenta(String tipoCuenta) {
+        this.tipoCuenta = tipoCuenta;
     }
 
-    public void setDniTitular(long dniTitular) {
-        this.dniTitular = dniTitular;
-    }
-
-    public String getNombreTitular() {
-        return nombreTitular;
-    }
-
-    public void setNombreTitular(String nombreTitular) {
-        this.nombreTitular = nombreTitular;
-    }
-
-    public String getApellidoTitular() {
-        return apellidoTitular;
-    }
-
-    public void setApellidoTitular(String apellidoTitular) {
-        this.apellidoTitular = apellidoTitular;
-    }
+    public void setBalance(double balance) {this.balance = balance;}
 }
